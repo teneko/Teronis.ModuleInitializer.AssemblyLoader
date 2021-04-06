308ba9689d854504704c14511c2d1cd4d770fe1d
teroneko <giteroneko@gmail.com>
2021-04-07 00:20:49 +0100

    Initial commit - Teronis.DotNet build dependencies added

---
+++ src/0/src/Utils/ExceptionUtils.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Teroneko.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teronis.ModuleInitializer.AssemblyLoader.Utils
+{
+    public static class ExceptionUtils
+    {
+        public static string GetMessageWithPrependedType(Exception error, string? message = null) =>
+            $"{(message == null ? null : $"{message} ")}{error.GetType()}: {error.Message}";
+    }
+}
